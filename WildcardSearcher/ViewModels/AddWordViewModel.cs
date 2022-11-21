26387221e4a552cc5e0287106cfa52e51f032ee8
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-11-21 22:59:25 +0700

    Added add word window

---
+++ WildcardSearcher/ViewModels/AddWordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace WildcardSearcher.ViewModels
+{
+    [INotifyPropertyChanged]
+    public partial class AddWordViewModel
+    {
+        public event Action AcceptRequested;
+
+        [ObservableProperty]
+        private string _word;
+
+        public AddWordViewModel()
+        {
+
+        }
+
+        [RelayCommand]
+        private void Accept()
+        {
+            AcceptRequested?.Invoke();
+        }
+    }
+}
---
+++ WildcardSearcher/Views/AddWordWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using WildcardSearcher.ViewModels;
+
+namespace WildcardSearcher.Views
+{
+    /// <summary>
+    /// Interaction logic for AddWordWindow.xaml
+    /// </summary>
+    public partial class AddWordWindow : Window
+    {
+        public AddWordViewModel ViewModel { get; }
+
+        public AddWordWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AddWordWindow(AddWordViewModel addWordViewModel) : this()
+        {
+            ViewModel = addWordViewModel;
+            DataContext = addWordViewModel;
+            ViewModel.AcceptRequested += ViewModel_AcceptRequested;
+        }
+
+        private void ViewModel_AcceptRequested()
+        {
+            DialogResult = true;
+        }
+    }
+}
