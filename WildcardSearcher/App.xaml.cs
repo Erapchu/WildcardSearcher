b95ca6814518059006c5f51c2e1eb290dc7f52dc
Andre <Andre@ANDREY>
2022-11-21 19:46:13 +0700

    Add project files.

---
+++ WildcardSearcher/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace WildcardSearcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+        }
+    }
+}
